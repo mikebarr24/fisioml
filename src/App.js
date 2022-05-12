20eae833f741d8bb4e1fded75931de3adc6aaade
Michael B <michaelbarr24@hotmail.com>
2022-05-12 08:40:56 +0100

    Add Home component and Add Font  -Set up the Home.js files  -Set up site font

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Home from "./components/home/Home";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/home/Home.js
@@ -0,0 +1,11 @@
+import "./Home.scss";
+
+function Home() {
+  return (
+    <section id="home">
+      <h1>Home</h1>
+    </section>
+  );
+}
+
+export default Home;
