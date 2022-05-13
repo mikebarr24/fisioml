ea5c0023ff6318bfcfd8afd079d527f4aef515f1
Michael B <michaelbarr24@hotmail.com>
2022-05-13 14:48:58 +0100

    Add info to all pages

--- src/pages/PilatesSuelo.js
+++ src/pages/PilatesSuelo.js
@@ -2,8 +2,28 @@ import "./PilatesSuelo.scss";
 
 function PilateSuelo() {
   return (
-    <div id="pilates-suelo">
-      <h1>Pilates Suelo</h1>
+    <div id="pilates-suelo" className="container">
+      <h1 className="page-title">Pilates Terapéutico Suelo</h1>
+      <p className="page-text">
+        Instructora certificada de Pilates Terapéutico Suelo por el método APPI
+        (The Australian Physiotherapy and Pilates Institute) en Reino Unido,
+        reconocido como líder en la industria de la enseñanza y desarrollo de
+        ejercicios seguros y efectivos, impartidos por expertos del movimiento.
+        <br />
+        <br />
+        Tu primera sesión será individual. Esta sesión tendrá como objetivo la
+        evaluación de tu postura y movilidad, así como la detección de posibles
+        disfunciones de fuerza, coordinación, propiocepción o control motor. En
+        esta sesión, María José te enseñará los cinco elementos clave del
+        Pilates. También aprenderás a activar la musculatura estabilizadora
+        necesaria para la realización de los ejercicios de forma segura y
+        eficaz.
+        <br />
+        <br />
+        Después de esta sesión, podrás continuar con sesiones individuales o en
+        grupos de 4 a 6 personas, tomando todas las medidas necesarias para la
+        seguridad frente al COVID-19. 
+      </p>
     </div>
   );
 }
