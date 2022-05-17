b70c6c5b227383d8e9d083aa1f23b4f349a0cc26
Michael B <michaelbarr24@hotmail.com>
2022-05-17 20:44:48 +0100

    Add hero button functionality

--- src/components/home/Hero/Hero.js
+++ src/components/home/Hero/Hero.js
@@ -1,17 +1,20 @@
 import Button from "../../default/Button";
 import "./Hero.scss";
-import { stickPilates } from "../../../imageFunctions";
+import { HashLink } from "react-router-hash-link";
 
 function Hero() {
-  const stickPics = stickPilates();
   return (
     <div id="hero" className="container">
       <div className="hero-text-wrapper">
         <h1>Te Ayudamos a Tomar el Control Sobre tu Salud</h1>
       </div>
       <div className="hero-buttons">
-        <Button name="Ver Sericios" className="hero-button" />
-        <Button name="Contacto" className="hero-button" />
+        <HashLink smooth to="#services">
+          <Button name="Ver Sericios" className="hero-button" />
+        </HashLink>
+        <HashLink smooth to="#contacto">
+          <Button name="Contacto" className="hero-button" />
+        </HashLink>
       </div>
     </div>
   );
